9acde3620778c65fc63f62f927be5880c8ccff02
Mahadeb Sen <mahadebsen237@gmail.com>
2022-07-29 01:16:26 +0530

    form design complete

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Form from "./Components/Form/Form";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h1 className="text-3xl font-bold underline">I am a leader</h1>
       <Form></Form>
     </div>
   );
